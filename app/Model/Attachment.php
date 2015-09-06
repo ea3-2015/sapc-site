9bd241322ad2f5592e7002ae4c8bd51832f2e506
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-09-05 22:44:06 -0430

    add: Configure models for Uploads.

---
+++ app/Model/Attachment.php
@@ -0,0 +1,101 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Genre Model
+ *
+ * @property Person $Person
+ */
+class Attachment extends AppModel
+{
+
+    public $actsAs = [
+        'Upload.Upload' => array(
+            'attachment' => array(
+                'thumbnailSizes' => array(
+                    // 'xvga' => '1024x768',
+                    'vga' => '640x480',
+                    'thumb' => '80x80',
+                ),
+            ),
+            'thumbnailMethod'=> 'php',
+            'fields' => array(
+                'dir' => 'dir'
+            )
+        ),
+    ];
+
+    /**
+ * Validation rules
+ *
+ * @var array
+ */
+    public $validate = array(
+        'model' => array(
+            'notEmpty' => array(
+                'rule' => array('notEmpty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'foreign_key' => array(
+            'numeric' => array(
+                'rule' => array('numeric'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'name' => array(
+            'notEmpty' => array(
+                'rule' => array('notEmpty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        /*'attachment' => array(
+            'notEmpty' => array(
+                'rule' => array('notEmpty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),*/
+        'size' => array(
+            'numeric' => array(
+                'rule' => array('numeric'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'active' => array(
+            'boolean' => array(
+                'rule' => array('boolean'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
+
+    public $belongsTo = [
+        'Noticia' => array(
+            'className' => 'Noticia',
+            'foreignKey' => 'foreign_key',
+        ),
+    ];
+}
--- app/Model/Noticia.php
+++ app/Model/Noticia.php
@@ -13,22 +13,7 @@ class Noticia extends AppModel {
  * @var array
  */
 	public $displayField = 'id';
-	public $actsAs = array(
-			'Upload.Upload' => array(
-				'foto'=> array ( 
-					'fields' => array(
-						'dir'=>'foto_dir'
-						),
-							'thumbnailMethod'=>'php',
-							'thumbnailSizes' => array(
-								'vga' => '400x400',
-								 'thumb'=>'100x100'
-								 ),
-								'deleteOnUpdate' => true,
-								'deleteFolderOnDelete' => true
-					   )
-				)
-		);
+
 	public $validate = array(
 		'title' => array(
 			'notBlank' => array(
@@ -68,4 +53,52 @@ class Noticia extends AppModel {
 			'order' => ''
 		)
 	);
+
+    public $hasMany = array(
+        'Image' => array(
+            'className' => 'Attachment',
+            'foreignKey' => 'foreign_key',
+            'conditions' => array(
+                'Image.model' => 'Noticia',
+            ),
+            'exclusive' => 'true',
+        ),
+    );
+
+    /**
+     * Crea la noticia con imágenes adjuntas.
+     *
+     * @param  array $data [description]
+     * @return bool
+     */
+    public function createWithAttachments(array $data)
+    {
+        // Sanitize your images before adding them
+        $images = array();
+        if (!empty($data['Image'][0])) {
+            foreach ($data['Image'] as $i => $image) {
+                if (is_array($data['Image'][$i])) {
+                    // Force setting the `model` field to this model
+                    $image['model'] = 'Noticia';
+
+                    // Unset the foreign_key if the user tries to specify it
+                    if (isset($image['foreign_key'])) {
+                        unset($image['foreign_key']);
+                    }
+
+                    $images[] = $image;
+                }
+            }
+        }
+        $data['Image'] = $images;
+
+        // Try to save the data using Model::saveAll()
+        $this->create();
+        if ($this->saveAll($data)) {
+            return true;
+        }
+
+        // Throw an exception for the controller
+        throw new Exception(__("This post could not be saved. Please try again"));
+    }
 }
