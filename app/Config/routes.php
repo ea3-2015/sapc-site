9e3d66c28f2a3726f6bf81a32060c634e099491d
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-09-13 10:07:59 -0430

    add: Enrutamiento de inicio y cierre de sesión.

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -30,6 +30,9 @@
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
+	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
+	Router::connect('/logout', array('controller' => 'users', 'action' => 'logout', 'admin' => true));
+
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
